e0dfedf5fad3e043edb6ea768567430f03ee5716
rl627 <rl627@drexel.edu>
2023-08-14 10:48:33 -0400

    brought in US specific spatial ETL pipeline  from kitchen sink #1

--- .etl/etl.R
+++ .etl/etl.R
@@ -1,81 +1,200 @@
-#' This script will generate the simulated data
+#' This script will generate the following outputs:
+#'   1. county_topo.json (legacy: geo_lad2021.json)
+#'   2. data_place.csv (legacy: data_region.csv)
+#'   2. data_county.csv (legacy: data_district.csv)
 
 { # Setup -------------------------------------------------------------------
   
   { # Dependencies ---------------------------------------------------------------
     library(tidyverse)
     library(geojsonio)  
-    library(jsonlite)
+    library(geoarrow)
+    library(glue)
+    library(curl)  
   }
+  
+  { ## UHC seeds ----------------------------------------------------
+    
+    uhc_api = 'https://github.com/Drexel-UHC/data-science/raw/main/etl/clean' 
+    sf_county_seed =  glue("{uhc_api}/boundaries/county.json") %>% topojson_read()
+    sf_place_seed =  glue("{uhc_api}/boundaries/place.json") %>% topojson_read()
+    curl_download(glue("{uhc_api}/df_demographics.parquet") , "processed/df_demographics.parquet")
+    curl_download(glue("{uhc_api}/xwalk_state.parquet") , "processed/xwalk_state.parquet")
+    df_demographics = arrow::read_parquet('processed/df_demographics.parquet')
+    xwalk_state = arrow::read_parquet('processed/xwalk_state.parquet')
+    
+  }
+  
 }
 
 
-{ # Original Line Data ---------------------------------------------------------------
+{ # EDA -----------------------------------------------------------
+  #' Examine and reproduce original items
   
-  df_line_raw = read_csv("../src/data/data_line.csv")
-  
-  df_line_raw %>% 
-    pivot_wider(names_from = group, values_from = value) %>% 
-    write.csv("../src/data/data_line_wide.csv")
-}
-
-
-
-
-
-
-
-  { # Longitudinal --------------------------------------------------------
+  { # geo_lad2021.json --------------------------------------------------------
     
-    { # Simulate data -----------------------------------------------------------
-      
-      df_us = read.csv("raw/NCHS_-_Death_rates_and_life_expectancy_at_birth.csv") %>% as_tibble() %>% 
-        janitor::clean_names() %>% 
-        filter(race == "All Races", sex == 'Both Sexes',
-               year > 1980) %>% 
-        select(year, 
-               le = average_life_expectancy_years) %>% 
-        mutate(group = 'US')
-      
-      df_dummy_cities = c("Chicago",'Boston') %>% 
-        map_df(function(city_tmp){
-          df_us %>% 
-            mutate(group = city_tmp,
-                   le = le + rnorm(n(), mean = rnorm(1, mean = 0, sd = 0.1), sd = 0.1))
-        })
-      df_dummy_cities %>% 
-        ggplot(aes(x =year, y = le, group = group, color = group))+
-        geom_line() 
-      
-      
-      df_philly =  read.csv("raw/le_gpt_philly.csv") %>% as_tibble() %>% 
-        mutate(group = "Philadelphia")
-      
-      df_data = list(df_philly, df_us, df_dummy_cities) %>% bind_rows()
-      
-      
-      df_data %>% 
-        ggplot(aes(x =year, y = le, group = group, color = group))+
-        geom_line() 
-      
-    }
-    
-    { # Tidy + export -----------------------------------------------------------
-
-      df_le = df_data %>% 
-        rename(value = 'le') %>%
-        mutate(group = group %>% recode(
-          'Boston' = 'apples',
-          'Chicago' = 'dates',
-          'US' = 'cherries',
-          'Philadelphia' = 'flowers' )) %>% 
-        arrange(group,year, value)
-      df_le_wide = df_le %>% 
-        pivot_wider(names_from = group, values_from = value)
-      df_le %>% write.csv("../public/data/data_le.csv", row.names = F)
-      df_le_wide %>% write.csv("../public/data/data_le_wide.csv", row.names = F)
-      
-    }
-    
+    sfa = geojsonio::topojson_read("../public/data/geo_lad2021.json")
+    glimpse(sfa)
+    sfa %>% geojsonio::topojson_write(
+      file = "../public/data/geo_lad2021_uhc.json",
+      object_name  = 'geog')
     
   }
+  
+  { # data_region.csv ------------------------------------------------------------------
+    ## region (12)
+    
+    df_region = read_csv("../public/data/data_region.csv")
+    df_region %>% glimpse()
+    df_region %>% count(code, name)  
+  }
+  
+  
+  { # data_district.csv -------------------------------------------------------
+    
+    ## district (374)
+    df_district = read_csv("../public/data/data_district.csv")
+    df_district %>% glimpse()
+    df_district %>% count(code, name)
+    
+  }
+  
+}
+
+{ # Outputs --------------------------------------------
+  
+  
+  region_tmp = 'Northeast'
+  division_tmp =  "Middle Atlantic"
+  
+  vec__state_abbr = xwalk_state %>% 
+    # filter(division_name == division_tmp) %>% 
+    filter(region_name == region_tmp) %>% 
+    pull(state_abbr)
+  
+  { ##  county_topo.json ----------------------------------------------------------------
+    
+    
+    ### Op
+    ### - select only PA, DE, MD
+    ### - rename as per original data structure
+    
+    sf_init = sf_county_seed %>% 
+      left_join(xwalk_state) %>% 
+      filter(state_abbr%in%vec__state_abbr)
+    
+    sf_uhc = sf_init %>%  
+      mutate(id = geoid) %>% 
+      select(id,
+             AREACD = geoid,
+             AREANM = county_name)
+    
+    ### Export
+    sf_uhc %>% 
+      geojsonio::topojson_write(
+        file = "../public/data/geo_counties.json",
+        object_name  = 'geog')
+    
+  }
+  
+  { # data_state.csv ----------------------------------------------------------
+    
+    df_region %>% glimpse()
+    
+    ## Intermediates
+    df_demographics_tmp = df_demographics %>% 
+      filter(geo == 'state') 
+    
+    df_pop_wide = df_demographics_tmp %>% 
+      select(-median_age) %>% 
+      pivot_wider(names_from = year, values_from = pop) %>% 
+      drop_na() %>% 
+      mutate(`2001` = 0,
+             `2002` = 0,
+             `2003` = 0,
+             `2004` = 0,
+             `2005` = 0,
+             `2006` = 0,
+             `2007` = 0,
+             `2008` = 0) %>% 
+      select(sort(names(.))) %>% 
+      select(geo, state_fip = geoid, geoid, everything())
+    
+ 
+    
+    ## Final
+    df_data_state = df_pop_wide %>% 
+      left_join(xwalk_state) %>% 
+      filter(state_abbr%in%vec__state_abbr) %>% 
+      select(code = state_fip,
+             name = state_name, 
+             # area = aland_mile2,
+             # density = pop_dens,
+             # age_med = median_age,
+             matches("\\b\\d{4}\\b"))
+    
+    ## Export
+    df_data_state %>% write_csv("../public/data/data_state.csv")
+    
+  }
+  
+  { # data_county.csv ----------------------------------------------------------
+    
+    df_district %>% glimpse()
+    
+    ## Intermediates
+    xwalk_county_state = sf_county_seed %>%
+      as.data.frame() %>% 
+      select(geoid, state_fip, county_name) %>% 
+      as_tibble()
+    
+    df_demographics_tmp = df_demographics %>% 
+      filter(geo == 'county') %>% 
+      left_join(xwalk_county_state) %>% 
+      left_join(xwalk_state) %>%
+      filter(state_abbr%in%vec__state_abbr) %>%
+      glimpse()
+    
+    df_pop_wide = df_demographics_tmp %>% 
+      select(-median_age) %>% 
+      pivot_wider(names_from = year, values_from = pop) %>% 
+      drop_na() %>% 
+      mutate(`2001` = 0,
+             `2002` = 0,
+             `2003` = 0,
+             `2004` = 0,
+             `2005` = 0,
+             `2006` = 0,
+             `2007` = 0,
+             `2008` = 0) %>% 
+      select(sort(names(.))) %>% 
+      select(geo, geoid, everything())
+    
+    df_spatial_metadata = sf_county_seed %>% 
+      as.data.frame() %>% 
+      as_tibble() %>% 
+      select(geoid, county_name,  aland_mile2,pop_dens) 
+    
+    df_age = df_demographics_tmp %>% 
+      filter(year == 2020) %>% 
+      select(geoid, median_age)
+    
+    ## Final
+    df_data_county = df_pop_wide %>% 
+      left_join(df_spatial_metadata) %>% 
+      left_join(df_age) %>% 
+      select(code = geoid,
+             name = county_name,
+             parent = state_fip,
+             area = aland_mile2,
+             density = pop_dens,
+             age_med = median_age,
+             matches("\\b\\d{4}\\b"))
+    
+    ## Export
+    df_data_county %>% write_csv("../public/data/data_county.csv")
+    
+  }
+  
+}
+
